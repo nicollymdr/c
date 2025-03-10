38a6ba491da96eea09cd1a7369239ced496d34a8
Nicolly Murgia dos Reis <134756022+nicollymdr@users.noreply.github.com>
2025-03-10 15:03:40 -0300

    saida-de-dados

---
+++ saida-de-dados/saida-de-dados.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main (){
+	
+	printf("Testando saída de dados. \n");
+	
+	printf("Valor inteiro: %d.\n", 10);
+	printf("Valor real: %f.\n", 1.854726);
+	printf("Valor real com apenas duas casas: %.2f.\n", 5.9874325);
+	printf("Dado de texto com um caracter: %c.\n", 'n');
+	printf("Dado de texto: %s.\n", "nicolly");
+	system("pause");
+	return 0;
+}
+
+
