15bfd08434cfda41ab5c478ece1a78e304feface
Nicolly Murgia dos Reis <134756022+nicollymdr@users.noreply.github.com>
2025-03-20 15:22:17 -0300

    feat

---
+++ do-while/do-while.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+	
+	int i = 1;
+	
+	do {
+		printf("Você comeu %d pedacos de pizza.\n", i);
+		i++;
+	} while (i <= 8);
+	
+	
+	system("pause");
+	return 0;
+}
---
+++ for/for.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+	
+	int i;
+	
+	for(i = 1; i <= 8; i++){
+		
+		printf("Voce comeu %d pedacos de pizza.\n",i);
+	}
+	
+	system("pause");
+	return 0;
+}
