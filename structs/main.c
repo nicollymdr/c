8dc88972cde4822fc9a104a24250dc390db37f29
Nicolly Murgia dos Reis <134756022+nicollymdr@users.noreply.github.com>
2025-07-21 23:09:57 -0300

    .

---
+++ structs/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include <locale.h>
+
+#define TAM 50
+
+struct tipo_pessoa{
+	int idade;
+	float peso;
+	char nome[TAM];
+};
+
+int main() {
+	setLocale(LC_ALL, "Portuguese");
+	
+	//Criando e inicializando
+	
+	return 0;
+}
